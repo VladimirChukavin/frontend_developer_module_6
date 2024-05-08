3dfde7e972473e6a4da518c97e2914c46b610e74
darthmorgot <dartmorgot@yandex.ru>
2024-05-09 04:01:24 +1000

    Добавил новый файл render-map.js.

---
+++ source/scripts/render-map.js
@@ -0,0 +1,49 @@
+/**
+ * Функция для скрытия маски в блоке map.
+ */
+const hideMask = () => {
+  const mapMask = document.querySelector('.map__mask');
+  mapMask.classList.add('toggle');
+};
+
+/**
+ * Функция для вывода карты на страницу.
+ */
+const renderMap = () => {
+  const map = L.map('map')
+    .on('load', () => {
+      hideMask();
+    })
+    .setView({
+      lat: 59.968322,
+      lng: 30.317359
+    }, 17);
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
+
+  const mainPinIcon = L.icon({
+    iconUrl: './images/map-pin.svg',
+    iconSize: [38, 50],
+    iconAnchor: [19, 50],
+  });
+
+  const mainPinMarker = L.marker(
+    {
+      lat: 59.968322,
+      lng: 30.317359
+    },
+    {
+      icon: mainPinIcon,
+      riseOnHover: true,
+    },
+  );
+
+  mainPinMarker.addTo(map);
+};
+
+export {renderMap};
