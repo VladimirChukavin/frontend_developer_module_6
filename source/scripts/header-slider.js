df1860a7f8fc1b0ba249141c2862e2d8e2717257
darthmorgot <dartmorgot@yandex.ru>
2024-05-10 05:01:46 +1000

    Добавил новый файл header-slider.js.

---
+++ source/scripts/header-slider.js
@@ -0,0 +1,17 @@
+const headerSlider = () => {
+  return new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+};
+
+export {headerSlider};
