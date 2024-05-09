38666b0353046720b99cf46408284a82afd1f350
darthmorgot <dartmorgot@yandex.ru>
2024-05-10 05:00:16 +1000

    Добавил новый файл filter-slider.js.

---
+++ source/scripts/filter-slider.js
@@ -0,0 +1,46 @@
+/**
+ * Функция для вывода на страницу в блоке каталога фильтра для выбора диапазона цен.
+ */
+const filterSlider = () => {
+  const slider = document.querySelector('#filter__slider');
+  const minPrice = document.querySelector('#price-lower');
+  const maxPrice = document.querySelector('#price-upper');
+  const reset = document.querySelector('.filter__button--reset');
+
+  noUiSlider.create(slider, {
+    start: [0, 900],
+    connect: true,
+    step: 1,
+    limit: 1000,
+    range: {
+      'min': 0,
+      'max': 1030
+    }
+  });
+
+  slider.noUiSlider.on('update', (values, handle) => {
+
+    const value = values[handle];
+
+    if (handle) {
+      maxPrice.value = Math.round(value);
+    } else {
+      minPrice.value = Math.round(value);
+    }
+  });
+
+  minPrice.addEventListener('change', () => {
+    slider.noUiSlider.set([this.value, null]);
+  });
+
+  maxPrice.addEventListener('change', () => {
+    slider.noUiSlider.set([null, this.value]);
+  });
+
+  reset.addEventListener('click', () => {
+    slider.noUiSlider.set([0, 900]);
+  });
+
+};
+
+export {filterSlider};
