076f4a44fd84985d15f22fabed8a7c3508b9fa94
darthmorgot <dartmorgot@yandex.ru>
2024-05-10 05:04:06 +1000

    Добавил импорты функций из новых файлов.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,9 @@
 import {mobileMenuManagement} from './mobile-menu-management.js';
 import {renderMap} from './render-map.js';
+import {filterSlider} from './filter-slider.js';
+import {headerSlider} from './header-slider.js';
 
 mobileMenuManagement();
 renderMap();
+filterSlider();
+headerSlider();
