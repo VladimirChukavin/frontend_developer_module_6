955e593eaa3116dca288189ccfb0b8e109b93d73
darthmorgot <dartmorgot@yandex.ru>
2024-05-10 13:11:38 +1000

    Мелкие правки кода, добавил описание.

--- source/scripts/header-slider.js
+++ source/scripts/header-slider.js
@@ -1,17 +1,18 @@
-const headerSlider = () => {
-  return new Swiper(".swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-};
+/**
+ * Функция инициализации слайдера Swiper.
+ */
+const headerSlider = () => new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 export {headerSlider};
